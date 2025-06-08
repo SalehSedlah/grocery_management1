62239c489e39a26686fba094b3b879f758031421
SalehSedlah <109546651+SalehSedlah@users.noreply.github.com>
2025-06-09 02:30:38 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-   output: 'export',
+   // output: 'export',
    i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'en',
